e575d70519b76b6240355ae2690e120a67d6a142
Asfand4321 <116038883+Asfand4321@users.noreply.github.com>
2026-01-09 23:29:09 +0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "https://vineyqzdyiivnsxjljcey.supabase.co";
+const SUPABASE_URL = "https://vineyqzdyiivsxjljcey.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpbmV5cXpkeWlpdnN4amxqY2V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5NzQ4NjgsImV4cCI6MjA4MzU1MDg2OH0.DPFRvWZjfFGCyprXU8_HruTqL_QwaY3PT5rkvc5CImI";
 
